88c2ec258da8cf379d372cdd0b5bdff0ec572583
Ekaterina Shipunova <odegda@rtr22.ru>
2024-05-26 22:12:37 +0700

    закоминтила modul1

---
+++ modul1.py
@@ -0,0 +1,8 @@
+def max1(a,b):
+    if a>b:
+        return a
+    #else:            # т.к. return останавливает работу программы,   
+        #return b     # то можно else не писать
+    return(b)
+
+    
